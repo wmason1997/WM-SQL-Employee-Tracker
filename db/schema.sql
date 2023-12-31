3a2300a635b0f9d8470845c85ea248998c00a4e8
wmason1997 <williamcmason1997@gmail.com>
2023-12-31 14:31:06 -0800

    Added the boilerplate code for server.js

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    manager_id INT, -- could be NULL theoretically
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL,
