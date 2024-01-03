2fb58c256b83ea3fc526eee4bba2aab9fd09f871
wmason1997 <williamcmason1997@gmail.com>
2024-01-02 18:06:17 -0800

    Updated schema.sql file with help from Michael in OH on 01/02/2024

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,33 @@
 -- Use or create the 'company_db' database
-CREATE DATABASE IF NOT EXISTS company_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
 USE company_db;
 
 -- Create tables according to the instructions of project
+-- DROP TABLE IF EXISTS department;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
+-- DROP TABLE IF EXISTS role;
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (7,0), -- TRYING BASED ON https://www.sqlshack.com/understanding-sql-decimal-data-type/
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
+-- DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT, -- could be NULL theoretically
     FOREIGN KEY (role_id)
     REFERENCES role(id)
