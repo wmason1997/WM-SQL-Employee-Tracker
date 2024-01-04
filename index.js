6f08aa1278c93c9f6ba03d8fa7ddcf21c925c2d9
wmason1997 <williamcmason1997@gmail.com>
2024-01-04 05:53:29 -0800

    Mila helped me to write ADD DEPARTMENT, ADD ROLE, and ADD EMPLOYEE. Now, I need to figure out Update Employee Role and keep inquirer prompts from shutting down.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function viewAllEmployees() {
     });
 }
 
-function viewAllDepartments() {
+async function viewAllDepartments() {
   return fs.readFile(path.join(__dirname, 'db', 'viewAllDepartments.sql'), 'utf8')
   .then((querySql) => {
     return connection.promise().query(querySql); // Use promise().query for promises
@@ -30,6 +30,75 @@ function viewAllRoles() {
     return connection.promise().query(querySql); // Use promise().query for promises
   });
 }
+
+function addDepartment() {
+  return prompt([
+    {
+      type: "input",
+      name: "departmentName",
+      message: "What is the name of the department?",
+    }
+  ]).then(async answers => { 
+    await connection.promise().query(`INSERT INTO department (name) VALUES ("${answers.departmentName}")`);
+    return viewAllDepartments();
+    })
+}
+
+async function addRole() {
+  const [departments] = await connection.promise().query('SELECT * FROM department');
+  return prompt([
+    {
+      type: "input",
+      name: "roleName",
+      message: "What is the name of the role?",
+    },
+    {
+      type: "input",
+      name: "roleSalary",
+      message: "What is the salary of the role?",
+    },
+    {
+      type: "list",
+      name: "roleDepartment",
+      choices: departments.map(({id, name}) => ({value: id, name})),
+    }
+  ]).then(async answers => { 
+    await connection.promise().query(`INSERT INTO role (title, salary, department_id) VALUES ("${answers.roleName}", "${answers.roleSalary}", ${answers.roleDepartment})`);
+    return viewAllRoles();
+    })
+}
+
+async function addEmployee() {
+  const [employees] = await connection.promise().query('SELECT * FROM employee');
+  const [roles] = await connection.promise().query('SELECT * FROM role');
+  return prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "What is the employee's first name?",
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "What is the employee's last name?",
+    },
+    {
+      type: "list",
+      name: "employeeRole",
+      choices: roles.map(({id, title}) => ({value: id, name: title})),
+      message: "What is the employee's role?",
+    },
+    {
+      type: "list",
+      name: "employeeManager",
+      choices: employees.map(({id, first_name, last_name}) => ({value: id, name: `${first_name} ${last_name}`})),
+      message: "Who is the employee's manager?",
+    }
+  ]).then(async answers => { 
+    await connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answers.firstName}", "${answers.lastName}", ${answers.employeeRole}, ${answers.employeeManager})`);
+    return viewAllEmployees();
+    })
+};
 
 function handlePromptChoice(promptChoice) {
   switch (promptChoice) {
@@ -57,8 +126,26 @@ function handlePromptChoice(promptChoice) {
 
     // "Add Department" case
     // case "Add Department":
+    case "Add Department":
+      return addDepartment().then((results) => {
+        console.log('All Departments:');
+        console.table(results[0]);
+      });
 
-    
+    // "Add Role" case
+    // case "Add Role":
+    case "Add Role":
+      return addRole().then((results) => {
+        console.log('All Roles:');
+        console.table(results[0]);
+      });
+
+    // "Add Employee" case
+    case "Add Employee":
+      return addEmployee().then((results) => {
+        console.log('All Employees:');
+        console.table(results[0]);
+      });
     // Handle other prompt choices here
 
     default:
